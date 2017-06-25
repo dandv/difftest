67a1b74e4a611102e42e180a770c7b15e15a4230
Dan Dascalescu <ddascalescu+github@gmail.com>
2017-06-24 23:40:01 -0700

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,12 @@
+/* Welcome to TestDiff!
+ *
+ * 
+ * Why do we stick with asterisks at the begining of each line? 
+ * This is my home page. To start with administering the wikis, please log in with the account admin/admin. Once you login you will be able to configure yours news wikis. If you want to play around with me a little, just create a New Page? or edit this one through the edit link at the top of the page.
+ * Need some assistance?
+ */
+ 
+ function foo() {
+   return 'foo';
+ }
+ 
