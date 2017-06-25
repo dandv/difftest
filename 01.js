8b05ff8946fc12fb6d479d643fbb4e95681049e5
Dan Dascalescu <ddascalescu+github@gmail.com>
2017-06-24 23:41:15 -0700

    Add one word in a long comment line

--- 01.js
+++ 01.js
@@ -2,7 +2,7 @@
  *
  * 
  * Why do we stick with asterisks at the begining of each line? 
- * This is my home page. To start with administering the wikis, please log in with the account admin/admin. Once you login you will be able to configure yours news wikis. If you want to play around with me a little, just create a New Page? or edit this one through the edit link at the top of the page.
+ * This is my front page. To start with administering the wikis, please log in with the account admin/admin. Once you login you will be able to configure yours news wikis. If you want to play around with me a little, just create a New Page? or edit this one through the edit link at the top of the page.
  * Need some assistance?
  */
  
